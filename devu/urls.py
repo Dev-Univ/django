da6785f1bf47ffd61fdddd8266471b499e61caec
YunJae00 <0109548@naver.com>
2024-12-17 03:29:42 +0900

    Fix: urls 복수형으로 변경

--- devu/urls.py
+++ devu/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include("user.urls")),
-    path('api/univ/', include("univ.urls")),
-    path('api/project/', include("project.urls")),
+    path('api/users/', include("user.urls")),
+    path('api/univs/', include("univ.urls")),
+    path('api/projects/', include("project.urls")),
 ]
